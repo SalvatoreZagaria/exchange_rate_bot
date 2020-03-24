6cff27ed530590712f97f1f2424f3195c70749ce
SalvatoreZagaria <37420335+SalvatoreZagaria@users.noreply.github.com>
2020-03-24 22:53:00 +0000

    SZ: Main thread

--- bot.py
+++ bot.py
@@ -7,30 +7,43 @@ from telegram.ext import Updater, CommandHandler
 from monitor import ExchangeRateMonitor, WEBSITE
 
 TOKEN = os.environ["EXC_RATE_TOKEN"]
-monitor_threads = {}
+CURRENT_VALUE = "No results found."
 
 
 class MonitorThread(threading.Thread):
+    def __init__(self, interval=300):
+        threading.Thread.__init__(self)
+        self.interval = interval
+        self.exc_rate_monitor = ExchangeRateMonitor(WEBSITE)
+
+    def run(self):
+        global CURRENT_VALUE
+        while True:
+            gbp_eur = self.exc_rate_monitor.process_rates()
+
+            if gbp_eur is None:
+                CURRENT_VALUE = "[!] {} RATES NOT FOUND".format(datetime.datetime.now())
+            else:
+                CURRENT_VALUE = "GBP - EUR: {}".format(gbp_eur)
+
+            time.sleep(self.interval)
+
+
+class SenderThread(threading.Thread):
     def __init__(self, updater, interval=60):
         threading.Thread.__init__(self)
-        self.exc_rate_monitor = ExchangeRateMonitor(WEBSITE)
         self.interval = interval
         self.updater = updater
         self.running = False
 
     def run(self):
         self.running = True
+        global CURRENT_VALUE
         while True:
             if not self.running:
                 break
-            gbp_eur = self.exc_rate_monitor.process_rates()
 
-            if gbp_eur is None:
-                msg = "[!] {} RATES NOT FOUND".format(datetime.datetime.now())
-            else:
-                msg = "GBP - EUR: {}".format(gbp_eur)
-
-            self.updater.message.reply_text(msg)
+            self.updater.message.reply_text(CURRENT_VALUE)
             time.sleep(self.interval)
 
     def stop(self):
@@ -40,49 +53,43 @@ class MonitorThread(threading.Thread):
         self.updater.message.reply_text(msg)
 
 
+MAIN_THREAD = MonitorThread()
+MAIN_THREAD.start()
+
+sender_threads = {}
+
+
 def get_one(update, context):
-    update.message.reply_text("Retrieving...")
-
-    exc_rate_monitor = ExchangeRateMonitor(WEBSITE)
-
-    gbp_eur = exc_rate_monitor.process_rates()
-
-    if gbp_eur is None:
-        msg = "[!] {} RATES NOT FOUND".format(datetime.datetime.now())
-    else:
-        msg = "GBP - EUR: {}".format(gbp_eur)
-
-    update.message.reply_text(msg)
+    global CURRENT_VALUE
+    update.message.reply_text(CURRENT_VALUE)
 
 
 def start(update, context):
-    update.message.reply_text("Retrieving...")
-
     user_id = update.effective_user.id
-    monitor_thread = monitor_threads.get(user_id, None)
-    if monitor_thread is not None and monitor_thread.running:
+    sender_thread = sender_threads.get(user_id, None)
+    if sender_thread is not None and sender_thread.running:
         update.message.reply_text("Bot is running already.")
         return
-    monitor_thread = MonitorThread(update)
-    monitor_thread.start()
-    monitor_threads[user_id] = monitor_thread
+    sender_thread = SenderThread(update)
+    sender_thread.start()
+    sender_threads[user_id] = sender_thread
 
 
 def stop(update, context):
     user_id = update.effective_user.id
-    monitor_thread = monitor_threads.get(user_id, None)
-    if monitor_thread is None or (monitor_thread is not None and not monitor_thread.running):
+    sender_thread = sender_threads.get(user_id, None)
+    if sender_thread is None or (sender_thread is not None and not sender_thread.running):
         update.message.reply_text("Bot is stopped already.")
         return
-    monitor_thread.stop()
-    monitor_threads[user_id] = None
+    sender_thread.stop()
+    sender_threads[user_id] = None
 
     update.message.reply_text("Stopped.")
 
 
 def set_interval(update, context):
     user_id = update.effective_user.id
-    monitor_thread = monitor_threads.get(user_id, None)
+    sender_thread = sender_threads.get(user_id, None)
 
     args = context.args
     if len(args) != 1:
@@ -94,22 +101,22 @@ def set_interval(update, context):
             update.message.reply_text("The new interval must be a number higher than 0.")
 
         run = False
-        if monitor_thread is not None and monitor_thread.running:
-            monitor_thread.stop()
+        if sender_thread is not None and sender_thread.running:
+            sender_thread.stop()
             run = True
-        elif monitor_thread is None or (monitor_thread is not None and not monitor_thread.running):
+        elif sender_thread is None or (sender_thread is not None and not sender_thread.running):
             update.message.reply_text("Bot must be running.")
             return
 
-        monitor_thread = MonitorThread(update, interval=new_interval * 60)
+        sender_thread = SenderThread(update, interval=new_interval * 60)
 
         msg = "Interval updated to {}".format(new_interval)
         update.message.reply_text(msg)
         if run:
             update.message.reply_text("Retrieving...")
-            monitor_thread.start()
+            sender_thread.start()
 
-        monitor_threads[user_id] = monitor_thread
+        sender_threads[user_id] = sender_thread
     except ValueError:
         update.message.reply_text("Impossible to convert argument to integer.")
 
@@ -147,7 +154,6 @@ if __name__ == "__main__":
         print("Bot is running...")
     except:
         shutting_down = "Bot is shutting down."
-        for th in monitor_threads.values():
+        for th in sender_threads.values():
             th.send_message(shutting_down)
-            th.stop()
         print(shutting_down)
